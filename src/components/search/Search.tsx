42249cc8688a835cf59282251f4e55539b9d9236
fernandoAvargas <fernan.assis@hotmail.com>
2023-02-10 01:34:15 -0300

    Realizar pesquisa ao clicar a tecla Enter

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,7 +2,7 @@ type SearchPorps = {
     loadUser: (userName: string) => Promise<void>;
 };
 
-import{useState} from "react";
+import{useState, KeyboardEvent} from "react";
 
 import {BsSearch} from 'react-icons/bs'
 
@@ -11,12 +11,21 @@ import styles from "./Search.module.css";
 const Search = ({loadUser}: SearchPorps) =>{
     const [userName,setUserName] = useState("");
 
+const handleKeyDown = (e : KeyboardEvent) => {
+    if(e.key === "Enter"){
+        loadUser(userName)
+    }
+}
+
     return(
         <div className={styles.search}>
             <h2>Busque por um usuário:</h2>
             <p>Conheça seus melhores repositórios</p>
             <div className={styles.search_container}>
-                <input type="text" placeholder="Digite o nome do usuário" onChange={(e) => setUserName(e.target.value)} />
+                <input type="text" 
+                placeholder="Digite o nome do usuário" 
+                onChange={(e) => setUserName(e.target.value)} 
+                onKeyDown={handleKeyDown}/>
                 <button onClick={()=> loadUser(userName)}>
                   <BsSearch />
                 </button>
